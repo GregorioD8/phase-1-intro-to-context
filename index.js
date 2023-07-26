ef3a7ca051f8d667af4f563a219b61ee002d3e5d
Gregorio <Greg.del801@gmail.com>
2023-07-26 15:32:08 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-// Your code here
+function createEmployeeRecord(employeeArray) {
+    let employee = {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+
+    }
+    return employee
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+
+    let employeeRecords = arrayOfArrays.map(array => createEmployeeRecord(array))
+    return employeeRecords
+}
+
+function createTimeInEvent(employeeObj, dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+
+    let timeInRecord = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    }
+    employeeObj.timeInEvents.push(timeInRecord)
+    return employeeObj
+
+}
+
+function createTimeOutEvent(employeeObj, dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+
+    let timeOutRecord = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    }
+    employeeObj.timeOutEvents.push(timeOutRecord)
+    return employeeObj
+
+}
+
+function hoursWorkedOnDate(employeeObj, dateWorked) {
+
+    let timeIn = employeeObj.timeInEvents.find(event => event.date === dateWorked)
+    let timeOut = employeeObj.timeOutEvents.find(event => event.date === dateWorked)
+    let hoursWorked = (timeOut.hour - timeIn.hour) / 100
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(employeeObj, dateWorked) {
+
+    let payOwed = (employeeObj.payPerHour) * (hoursWorkedOnDate(employeeObj, dateWorked))
+
+    return payOwed
+}
+
+function allWagesFor(employee) {
+
+    let eligibleDates = employee.timeInEvents.map(events => events.date)
+    let payable = eligibleDates.reduce((accumulator, currentValue) => accumulator + wagesEarnedOnDate(employee, currentValue), 0)
+
+    return payable
+}
+
+function calculatePayroll(employeeRecordsArray) {
+
+    let payroll = employeeRecordsArray.reduce((accumulator, currentValue) => accumulator + allWagesFor(currentValue), 0)
+
+    return payroll
+}
